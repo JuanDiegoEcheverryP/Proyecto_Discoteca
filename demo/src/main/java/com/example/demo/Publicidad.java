0f92ac67d7937273332355ecf42eef28e561a334
AndresCRincon <124530465+AndresCRincon@users.noreply.github.com>
2023-03-13 12:55:34 -0500

    Update Publicidad.java

--- demo/src/main/java/com/example/demo/Publicidad.java
+++ demo/src/main/java/com/example/demo/Publicidad.java
@@ -3,45 +3,22 @@ package com.example.demo;
 
 import java.util.Date;
 
-/**
-* @generated
-*/
 public class Publicidad {
     
-    /**
-    * @generated
-    */
+
     private Integer idDiscoteca;
-    
-    /**
-    * @generated
-    */
+
     private Integer idPublicidad;
-    
-    /**
-    * @generated
-    */
+
     private Date FechaInicio;
-    
-    /**
-    * @generated
-    */
+
     private Date FechaFinal;
-    
-    /**
-    * @generated
-    */
+
     private Boolean Estado;
     
-    /**
-    * @generated
-    */
     private DataType Imagen;
     
-    
-    /**
-    * @generated
-    */
+
     private Discoteca discoteca;
 
     public Publicidad(Integer idDiscoteca, Integer idPublicidad, Date fechaInicio, Date fechaFinal, Boolean estado, DataType imagen, Discoteca discoteca) {
@@ -54,86 +31,50 @@ public class Publicidad {
         this.discoteca = discoteca;
     }
 
-    /**
-    * @generated
-    */
     private Integer getIdDiscoteca() {
         return this.idDiscoteca;
     }
-    
-    /**
-    * @generated
-    */
+
     private Integer setIdDiscoteca(Integer idDiscoteca) {
         return this.idDiscoteca = idDiscoteca;
     }
     
-    /**
-    * @generated
-    */
     private Integer getIdPublicidad() {
         return this.idPublicidad;
     }
-    
-    /**
-    * @generated
-    */
+
     private Integer setIdPublicidad(Integer idPublicidad) {
         return this.idPublicidad = idPublicidad;
     }
-    
-    /**
-    * @generated
-    */
+
     private Date getFechaInicio() {
         return this.FechaInicio;
     }
     
-    /**
-    * @generated
-    */
     private Date setFechaInicio(Date FechaInicio) {
         this.FechaInicio = FechaInicio;
     }
-    
-    /**
-    * @generated
-    */
+
     private Date getFechaFinal() {
         return this.FechaFinal;
     }
-    
-    /**
-    * @generated
-    */
+
     private Date setFechaFinal(Date FechaFinal) {
         return this.FechaFinal = FechaFinal;
     }
-    
-    /**
-    * @generated
-    */
+
     private Boolean getEstado() {
         return this.Estado;
     }
-    
-    /**
-    * @generated
-    */
+
     private Boolean setEstado(Boolean Estado) {
         this.Estado = Estado;
     }
-    
-    /**
-    * @generated
-    */
+
     private DataType getImagen() {
         return this.Imagen;
     }
-    
-    /**
-    * @generated
-    */
+
     private DataType setImagen(DataType Imagen) {
         this.Imagen = Imagen;
     }
